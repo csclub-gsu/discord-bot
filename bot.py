19612d591dd9d835bcab7a1b15ad438f0f0ce3c8
Joshua Thomas <105134280+jkthom@users.noreply.github.com>
2023-02-13 13:25:55 -0500

    Made initial framework for discord bot and updated .gitignore

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+import asyncio
+
+#read token.txt
+with open('token.txt', 'r') as f:
+    token = f.read()
+    
+def run_bot():
+    token = 'token'
+    intents = discord.Intents.all()
+    client = discord.Client(intents=intents)
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
+
+    @client.event
+    async def on_message(message):
+        if message.content.startswith('hello'):
+            msg = 'Hello {0.author.mention}'.format(message)
+            await message.channel.send(msg)
+
+    client.run(token)
